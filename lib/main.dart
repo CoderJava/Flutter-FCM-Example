a3fe5c2d1895f416d684073693eb84fbe80cb5e0
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-01-19 21:40:22 +0700

    Integration FCM for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,243 @@
+import 'dart:async';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+final Map<String, Item> _items = <String, Item>{};
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+Item _itemForMessage(Map<String, dynamic> message) {
+  final dynamic data = message['data'] ?? message;
+  final String itemId = data['id'];
+  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))..status = data['status'];
+  return item;
+}
+
+class Item {
+  final String itemId;
+
+  Item({this.itemId});
+
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+
+  Stream<Item> get onChanged => _controller.stream;
+
+  String _status;
+
+  String get status => _status;
+
+  set status(String status) {
+    this._status = status;
+    _controller.add(this);
+  }
+
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+      () => MaterialPageRoute<void>(
+        settings: RouteSettings(name: routeName),
+        builder: (BuildContext context) => DetailPage(itemId),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+void main() {
+  runApp(
+    MaterialApp(
+      home: PushMessagingExample(),
+    ),
+  );
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PushMessagingExample extends StatefulWidget {
+  @override
+  _PushMessagingExampleState createState() => _PushMessagingExampleState();
+}
 
-  void _incrementCounter() {
+class _PushMessagingExampleState extends State<PushMessagingExample> {
+  String _homeScreenText = 'Waiting for token...';
+  bool _topicButtonDisabled = false;
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final TextEditingController _topicController = TextEditingController(text: 'topic');
+
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      content: Text('Item ${item.itemId} has been updated'),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: Text('OPEN'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate) {
+        _navigateToItemDetail(message);
+      }
+    });
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    final Item item = _itemForMessage(message);
+    // Clear away dialogs
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+
+  @override
+  void initState() {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('onMessage: $message');
+        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onLaunch: $message');
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      setState(() {
+        _homeScreenText = 'Push Messaging token: $token';
+      });
+      print('token: $_homeScreenText');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter FCM'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showItemDialog(
+          <String, dynamic>{
+            'data': <String, String>{
+              'id': '2',
+              'status': 'out of stock',
+            },
+          },
+        ),
+        tooltip: 'Simulate Message',
+        child: Icon(Icons.message),
+      ),
+      body: Material(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text(_homeScreenText),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _topicController,
+                    onChanged: (String v) {
+                      setState(() {
+                        _topicButtonDisabled = v.isEmpty;
+                      });
+                    },
+                  ),
+                ),
+                FlatButton(
+                  child: Text('subscribe'),
+                  onPressed: _topicButtonDisabled
+                      ? null
+                      : () {
+                          _firebaseMessaging.subscribeToTopic(_topicController.text);
+                          _clearTopicText();
+                        },
+                ),
+                FlatButton(
+                  child: Text('Unsubscribe'),
+                  onPressed: _topicButtonDisabled
+                      ? null
+                      : () {
+                          _firebaseMessaging.unsubscribeFromTopic(_topicController.text);
+                          _clearTopicText();
+                        },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _clearTopicText() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _topicController.text = '';
+      _topicButtonDisabled = true;
+    });
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  final String itemId;
+
+  DetailPage(this.itemId);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Item _item;
+  StreamSubscription<Item> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((Item item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Item ${_item.itemId}'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Material(
+        child: Center(
+          child: Text('Item status: ${_item.status}'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
